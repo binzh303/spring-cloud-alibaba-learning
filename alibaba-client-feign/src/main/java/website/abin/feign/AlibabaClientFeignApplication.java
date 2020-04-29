dd183f58464d2e327a9a364552f779ad724c2284
zhoubin <binzh303@163.com>
2020-04-29 17:01:28 +0800

    feign调用示例

---
+++ alibaba-client-feign/src/main/java/website/abin/feign/AlibabaClientFeignApplication.java
@@ -0,0 +1,36 @@
+package website.abin.feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableFeignClients // 开启feign
+public class AlibabaClientFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaClientFeignApplication.class, args);
+    }
+
+    @RestController
+    class TestController{
+
+        @Autowired
+        Client client;
+        @GetMapping("/test")
+        String test(){
+
+            return client.hello("hahaha");
+        }
+    }
+    @FeignClient(name="nacos-discovery-server")
+    interface Client{
+        @GetMapping("/hello")
+        String hello(@RequestParam String name);
+    }
+}
---
+++ alibaba-client-feign/src/test/java/website/abin/feign/AlibabaClientFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package website.abin.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaClientFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
